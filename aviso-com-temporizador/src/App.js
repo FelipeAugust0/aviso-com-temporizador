e5bd49ec2d97b5c5f3508f8ee299ab1843c38e21
FelipeAugust0 <lipe.augusto2013@gmail.com>
2025-09-22 19:49:10 -0300

    NEW: temporizador

--- aviso-com-temporizador/src/App.js
+++ aviso-com-temporizador/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+// App.jsx
+import React from "react";
+import AvisoTemporizado from "./AvisoTemporizado";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Meu App</h1>
+      <AvisoTemporizado />
     </div>
   );
 }
